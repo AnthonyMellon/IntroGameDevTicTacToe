afca97559a5a6aed6cb39cc0403703e10e7c34ed
Anthony <anthonymellon@yahoo.com>
2023-06-22 23:22:01 +1200

    Placing crosses

--- My project/Assets/Scripts/BoardCell.cs
+++ My project/Assets/Scripts/BoardCell.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BoardCell : MonoBehaviour
 {
     public float size = 2.5f;
+    [SerializeField] private SpriteRenderer tile;
+    [SerializeField] private Sprite tempTileSprite;
+
+    private void OnMouseDown()
+    {
+        if(tile.sprite == null)
+        {
+            Debug.Log("Setting sprite");
+            tile.sprite = tempTileSprite;
+        }
+    }
 }
